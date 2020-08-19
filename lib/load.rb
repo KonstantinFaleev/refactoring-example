bf92ab1d6d2a8ed8b46d8b288311c8d681c14ba0
Konstantin <konstantin.faleev@gmail.com>
2020-08-19 17:04:57 +0300

    added load

--- lib/load.rb
+++ lib/load.rb
@@ -1,6 +1,30 @@
+# frozen_string_literal: true
+
+require 'i18n'
 require 'yaml'
 require 'pry'
-require 'i18n'
-require_relative 'config/i18n_config'
-require_relative 'console'
-require_relative 'account'
+require 'digest'
+
+require_relative '../config/app_config'
+
+require_relative 'helpers/db_helper'
+require_relative 'helpers/validation_helper'
+
+require_relative 'validators/card_validator'
+require_relative 'validators/account_validator'
+require_relative 'validators/transaction_validator'
+
+require_relative 'app/view'
+
+require_relative 'cards/base'
+require_relative 'cards/usual'
+require_relative 'cards/capitalist'
+require_relative 'cards/virtual'
+
+require_relative 'app/builder'
+require_relative 'app/account'
+require_relative 'app/console'
+
+require_relative '../initializers/i18n'
+
+I18n.config.load_path << Dir[File.expand_path('config/locales').concat('/*.yml')]
